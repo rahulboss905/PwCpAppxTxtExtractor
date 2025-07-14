e8f2a0b76a17c17f93d43f578d11ae67bd3970df
rahulboss905 <166490010+rahulboss905@users.noreply.github.com>
2025-07-14 21:47:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "API_ID"
+api_hash = "API_HASH"
+bot_token = "BOT_TOKEN"
+auth_users = [AUTH_USERS]
